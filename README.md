08e9d14606494788588825e251a85d81ab81ef49
Brian Takita <brian.takita@gmail.com>
2024-04-10 00:48:20 -0400

    + index.js
    + prime_WeakRef.js

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # weakref-benchmark
 Demonstrates WeakRef performance issues with nodejs
+
+```sh
+npm i
+bun index.js
+node index.js
+```
+
+## index.js
+```
+bun index.js
+fake_WeakRef__memo_ x 759,429 ops/sec ±1.82% (88 runs sampled)
+eager_WeakRef__memo_ x 681,268 ops/sec ±1.96% (90 runs sampled)
+lazy_WeakRef__memo_ x 630,144 ops/sec ±1.42% (91 runs sampled)
+Fastest is fake_WeakRef__memo_
+```
+
+```
+node index.js
+fake_WeakRef__memo_ x 430,406 ops/sec ±19.22% (46 runs sampled)
+eager_WeakRef__memo_ x 69,115 ops/sec ±46.56% (38 runs sampled)
+lazy_WeakRef__memo_ x 23,938 ops/sec ±98.68% (28 runs sampled)
+Fastest is fake_WeakRef__memo_
+```
+
+## prime_WeakRef.js
+```
+bun prime_WeakRef.js
+WeakRef x 50,363,731 ops/sec ±5.99% (67 runs sampled)
+fake_WeakRef__memo_ x 779,351 ops/sec ±1.97% (87 runs sampled)
+eager_WeakRef__memo_ x 695,815 ops/sec ±1.10% (92 runs sampled)
+lazy_WeakRef__memo_ x 624,601 ops/sec ±1.96% (92 runs sampled)
+Fastest is WeakRef
+```
+
+```
+node prime_WeakRef.js
+WeakRef x 1,506,092 ops/sec ±16.04% (23 runs sampled)
+fake_WeakRef__memo_ x 118,439 ops/sec ±21.99% (66 runs sampled)
+eager_WeakRef__memo_ x 56,684 ops/sec ±40.38% (64 runs sampled)
+lazy_WeakRef__memo_ x 22,026 ops/sec ±91.35% (29 runs sampled)
+Fastest is WeakRef
+```
---
+++ prime_WeakRef.js
@@ -0,0 +1,245 @@
+import Benchmark from 'benchmark'
+/**
+ * @type {{ c?:memo_T, q:Set<memo_T> }}
+ * $.c child memo calling the memo
+ * $.q queue
+ */
+let $ = globalThis.__rmemo__ ??= { q: new Set }
+suite__run()
+function suite__run() {
+	const suite = new Benchmark.Suite
+	suite// uncomment 'WeakRef' benchmark to see following benchmarks degrade
+		.add('WeakRef', ()=>{
+			const o = {}
+			const wr = new WeakRef(o)
+			wr.deref()
+		})
+		.add('fake_WeakRef__memo_', ()=>{
+			const a$ = fake_WeakRef__memo_(()=>1)
+			const b$ = fake_WeakRef__memo_(()=>a$() + 10)
+			const c$ = fake_WeakRef__memo_(()=>b$() + 10)
+			const d$ = fake_WeakRef__memo_(()=>c$() + 10)
+			const e$ = fake_WeakRef__memo_(()=>d$() + 10)
+			const f$ = fake_WeakRef__memo_(()=>e$() + 10)
+			const g$ = fake_WeakRef__memo_(()=>f$() + 10)
+			const h$ = fake_WeakRef__memo_(()=>g$() + 10)
+			h$()
+		})
+		.add('eager_WeakRef__memo_', ()=>{
+			const a$ = eager_WeakRef__memo_(()=>1)
+			const b$ = eager_WeakRef__memo_(()=>a$() + 10)
+			const c$ = eager_WeakRef__memo_(()=>b$() + 10)
+			const d$ = eager_WeakRef__memo_(()=>c$() + 10)
+			const e$ = eager_WeakRef__memo_(()=>d$() + 10)
+			const f$ = eager_WeakRef__memo_(()=>e$() + 10)
+			const g$ = eager_WeakRef__memo_(()=>f$() + 10)
+			const h$ = eager_WeakRef__memo_(()=>g$() + 10)
+			h$()
+		})
+		.add('lazy_WeakRef__memo_', ()=>{
+			const a$ = lazy_WeakRef__memo_(()=>1)
+			const b$ = lazy_WeakRef__memo_(()=>a$() + 10)
+			const c$ = lazy_WeakRef__memo_(()=>b$() + 10)
+			const d$ = lazy_WeakRef__memo_(()=>c$() + 10)
+			const e$ = lazy_WeakRef__memo_(()=>d$() + 10)
+			const f$ = lazy_WeakRef__memo_(()=>e$() + 10)
+			const g$ = lazy_WeakRef__memo_(()=>f$() + 10)
+			const h$ = lazy_WeakRef__memo_(()=>g$() + 10)
+			h$()
+		})
+		.on('cycle', event=>{
+			console.log(String(event.target))
+		})
+		.on('complete', function() {
+			console.log('Fastest is ' + this.filter('fastest').map('name'))
+		})
+		.run()
+}
+/**
+ * @param {memo_def_T}memo_def
+ * @param {rmemo_add_def_T[]}add_def_a1
+ * @returns {memo_T}
+ * @private
+ */
+export function lazy_WeakRef__memo_(memo_def, add_def_a1) {
+	/** @type {memo_T} */
+	let memo = ()=>{
+		'val' in memo || memo__run(memo)
+		if ($.c) {
+			if (!memo.r.includes($.c.s ??= new WeakRef($.c))) {
+				memo.r.push($.c.s)
+			}
+			if ($.c.l < memo.l + 1) $.c.l = memo.l + 1
+			// memo called by $.c's conditional execution...next change to memo will notify $.c
+			$.c.u.push(memo)
+			// prevent memo from GC while $.c still has a strong reference
+			if (!$.c.t.includes(memo)) $.c.t.push(memo)
+		}
+		return memo.val
+	}
+	memo.set = val=>{
+		if (memo.val !== val) {
+			memo.r = memo.r.filter(r=>{
+				r = r.deref()
+				if (r?.u.includes(memo)) { // if added by $.c.u.push(memo), add to $.q
+					$.q.add(r)
+				}
+				return r
+			})
+		}
+		memo.val = val
+		if (!memo.a) {
+			memo.a = []
+			add_def_a1.map(add_def=>{
+				let v = add_def(memo)
+				if (v instanceof Object) {
+					memo.a.push(v)
+					if (v.memo_) v()
+				}
+			})
+		}
+		cur_refresh_loop:for (let cur_memo of $.q) {
+			$.q.delete(cur_memo)
+			for (let queue_refresh of $.q) {
+				if (cur_memo.l > queue_refresh.l) {
+					$.q.add(cur_memo)
+					continue cur_refresh_loop
+				}
+			}
+			memo__run(cur_memo)
+		}
+	}
+	memo.l = 0
+	memo.d = memo_def
+	memo.b = add_def_a1 ??= []
+	memo.r = []
+	memo.u = []
+	memo.t = []
+	return memo
+}
+export function eager_WeakRef__memo_(memo_def, add_def_a1) {
+	/** @type {memo_T} */
+	let memo = ()=>{
+		'val' in memo || memo__run(memo)
+		if ($.c) {
+			if (!memo.r.includes($.c.s)) {
+				memo.r.push($.c.s)
+			}
+			if ($.c.l < memo.l + 1) $.c.l = memo.l + 1
+			// memo called by $.c's conditional execution...next change to memo will notify $.c
+			$.c.u.push(memo)
+			// prevent memo from GC while $.c still has a strong reference
+			if (!$.c.t.includes(memo)) $.c.t.push(memo)
+		}
+		return memo.val
+	}
+	memo.set = val=>{
+		if (memo.val !== val) {
+			memo.r = memo.r.filter(r=>{
+				r = r.deref()
+				if (r?.u.includes(memo)) { // if added by $.c.u.push(memo), add to $.q
+					$.q.add(r)
+				}
+				return r
+			})
+		}
+		memo.val = val
+		if (!memo.a) {
+			memo.a = []
+			add_def_a1.map(add_def=>{
+				let v = add_def(memo)
+				if (v instanceof Object) {
+					memo.a.push(v)
+					if (v.memo_) v()
+				}
+			})
+		}
+		cur_refresh_loop:for (let cur_memo of $.q) {
+			$.q.delete(cur_memo)
+			for (let queue_refresh of $.q) {
+				if (cur_memo.l > queue_refresh.l) {
+					$.q.add(cur_memo)
+					continue cur_refresh_loop
+				}
+			}
+			memo__run(cur_memo)
+		}
+	}
+	memo.l = 0
+	memo.s = new WeakRef(memo)
+	memo.d = memo_def
+	memo.b = add_def_a1 ??= []
+	memo.r = []
+	memo.u = []
+	memo.t = []
+	return memo
+}
+export function fake_WeakRef__memo_(memo_def, add_def_a1) {
+	/** @type {memo_T} */
+	let memo = ()=>{
+		'val' in memo || memo__run(memo)
+		if ($.c) {
+			// if (!memo.r.includes($.c.s ??= new WeakRef($.c))) {
+			let c = $.c
+			if (!memo.r.includes($.c.s ??= { deref: ()=>c })) {
+				memo.r.push($.c.s)
+			}
+			if ($.c.l < memo.l + 1) $.c.l = memo.l + 1
+			// memo called by $.c's conditional execution...next change to memo will notify $.c
+			$.c.u.push(memo)
+			// prevent memo from GC while $.c still has a strong reference
+			if (!$.c.t.includes(memo)) $.c.t.push(memo)
+		}
+		return memo.val
+	}
+	memo.set = val=>{
+		if (memo.val !== val) {
+			memo.r = memo.r.filter(r=>{
+				r = r.deref()
+				if (r?.u.includes(memo)) { // if added by $.c.u.push(memo), add to $.q
+					$.q.add(r)
+				}
+				return r
+			})
+		}
+		memo.val = val
+		if (!memo.a) {
+			memo.a = []
+			add_def_a1.map(add_def=>{
+				let v = add_def(memo)
+				if (v instanceof Object) {
+					memo.a.push(v)
+					if (v.memo_) v()
+				}
+			})
+		}
+		cur_refresh_loop:for (let cur_memo of $.q) {
+			$.q.delete(cur_memo)
+			for (let queue_refresh of $.q) {
+				if (cur_memo.l > queue_refresh.l) {
+					$.q.add(cur_memo)
+					continue cur_refresh_loop
+				}
+			}
+			memo__run(cur_memo)
+		}
+	}
+	memo.l = 0
+	memo.d = memo_def
+	memo.b = add_def_a1 ??= []
+	memo.r = []
+	memo.u = []
+	memo.t = []
+	return memo
+}
+function memo__run(memo) {
+	let prev_memo = $.c
+	$.c = memo
+	memo.u = [] // reset references in memo_def conditional execution path...see $.c.u.push(memo)
+	try {
+		memo.set(memo.d(memo))
+	} catch (err) {
+		console.error(err)
+	}
+	$.c = prev_memo // catch does not throw
+}27
